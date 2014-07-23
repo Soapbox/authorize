556f1296db7dd8c09f6986c371af963476736278
Jaspaul Bola <jaspaul.b@gmail.com>
2014-07-23 10:51:02 -0400

    Refactor to remove getUser

--- src/SoapBox/Authorize/Strategies/OAuth2Strategy.php
+++ src/SoapBox/Authorize/Strategies/OAuth2Strategy.php
@@ -19,22 +19,9 @@ abstract class OAuth2Strategy implements Strategy {
 	 * @throws AuthenticationException If the provided parameters do not
 	 *	successfully authenticate.
 	 *
-	 * @return bool Indicates if login was successful
-	 */
-	public abstract function login($parameters = array());
-
-	/**
-	 * Used to retrieve a user using this Strategy.
-	 *
-	 * @param array parameters The parameters requried to authenticate against
-	 *	this strategy. (i.e. username, password, etc)
-	 *
-	 * @throws AuthenticationException If you attempt to retrieve a user profile
-	 *	but do not pass authentication.
-	 *
 	 * @return User A mixed array repreesnting the authenticated user.
 	 */
-	public abstract function getUser($parameters = array());
+	public abstract function login($parameters = array());
 
 	/**
 	 * Used to handle tasks after login. This could include retrieving our users
--- src/SoapBox/Authorize/Strategy.php
+++ src/SoapBox/Authorize/Strategy.php
@@ -19,21 +19,8 @@ interface Strategy {
 	 * @throws AuthenticationException If the provided parameters do not
 	 *	successfully authenticate.
 	 *
-	 * @return bool Indicates if login was successful
-	 */
-	public function login($parameters = array());
-
-	/**
-	 * Used to retrieve a user using this Strategy.
-	 *
-	 * @param array parameters The parameters requried to authenticate against
-	 *	this strategy. (i.e. username, password, etc)
-	 *
-	 * @throws AuthenticationException If you attempt to retrieve a user profile
-	 *	but do not pass authentication.
-	 *
 	 * @return User A mixed array repreesnting the authenticated user.
 	 */
-	public function getUser($parameters = array());
+	public function login($parameters = array());
 
 }
