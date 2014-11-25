722d88f96da00db5bf9b459c6c899301ab5d416d
Jaspaul Bola <jaspaul.b@gmail.com>
2014-11-25 12:19:34 -0500

    Remove unneeded helper methods for store / load

--- src/SoapBox/Authorize/Helpers.php
+++ src/SoapBox/Authorize/Helpers.php
@@ -7,8 +7,6 @@
 class Helpers {
 
 	public static $redirect;
-	public static $store;
-	public static $load;
 
 	/**
 	 * Helper to redirect the a given url
@@ -18,29 +16,6 @@ class Helpers {
 	public static function redirect($url) {
 		$redirect = Helpers::$redirect;
 		$redirect($url);
-	}
-
-	/**
-	 * Stores the provided value in our session
-	 *
-	 * @param string $key The dictionary location where we would like to store the value
-	 * @param mixed $value The value we wish to store
-	 */
-	public static function store($key, $value) {
-		$store = Helpers::$store;
-		$store($key, $value);
-	}
-
-	/**
-	 * Retrieves the previously stored value from our session
-	 *
-	 * @param string $key The key with which we have binded the value we are retrieving
-	 *
-	 * @return mixed The stored value
-	 */
-	public static function load($key) {
-		$load = Helpers::$load;
-		return $load($key);
 	}
 
 	/**
